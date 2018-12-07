9b122d10ce2e6c04f42aee6a32cd22a924690a74
sami.ch.27@gmail.com <sami.ch.27@gmail.com>
2018-12-07 18:42:57 +0100

    exemple d'un client en jquery

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,7 +19,8 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
 
             new FOS\RestBundle\FOSRestBundle(),
-	        new JMS\SerializerBundle\JMSSerializerBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
+            new Nelmio\CorsBundle\NelmioCorsBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
